25e32a19ff4eb61cbe0c3eba185ef1da3631cfe2
coke727 <jorge.cuadrado.saez@gmail.com>
2015-05-23 20:32:00 +0200

    Creada clase de datos generica usando patron singleton, añadirla metodos add para los diferentes ejercicios, simulador.java tiene ya creados listeners que cuando se pulsan los botones reciben los datos o limpian los campos o borran el ultimo suceso de la lista, borradas clases inecesarias

--- app/src/main/java/es/uva/inf/kave/myapplication/Datos.java
+++ app/src/main/java/es/uva/inf/kave/myapplication/Datos.java
@@ -14,65 +14,39 @@ import java.util.Calendar;
 @SuppressLint("SimpleDateFormat")
 public class Datos {
 
-	protected int apartado;
-	protected ArrayList<Medida> arrayDatos;
-	private boolean lleno;
-	
-	/**
-	 * Genera un nuevo contenido con Datos de un "apartado" determinado.
-	 * @param apartado Número del apartado al que serán dedicados los Datos.
-	 */
-	public Datos(int apartado) {
-		// TODO Auto-generated constructor stub
-		this.apartado = apartado;
-		this.arrayDatos = new ArrayList<Medida>();
-		lleno = false;
-	}
-	/**
-	 * Añade una Medida al conjunto de datos.
-	 * Es responsabilidad del usuario de esta clase, que
-	 * la el apartado al que corresponde la medida coincida
-	 * con el apartado de los Datos.
-	 * @param medida Medida con el mismo apartado que los Datos
-	 */
-	public void add_dato(Medida medida){
-		//TODO comprobar si esta la medida.
-		this.arrayDatos.add(medida);
-	}
-	
-	/**
-	 * Permite añadir los dos valores correspondientes a una medida al registro de Datos.
-	 * @param y Valor de la distancia (cm) o intensidad (mA) dependiendo del apartado de los Datos.
-	 * @param campo Valor del campo magnético correspondiente al valor "y".
-	 */
-	public void add_dato(double y, double campo){
-		if(apartado <= 1){
-			Medida medida = new Medida(y, campo, y*2);
-			this.arrayDatos.add(medida);
-		}else{//apartado >1
-			Medida medida = new Medida(y, campo);
-			this.arrayDatos.add(medida);
-		}
-	}
-	/**
-	 * 
-	 * @return Array de medidas de los Datos de un apartado indicado.
-	 */
-	public ArrayList<Medida> get_array(){
-		return arrayDatos;
+	private ArrayList<String> arrayMedidas;
+	private String medida;
+
+	private static Datos datos;
+
+	private Datos() {
+		arrayMedidas = new ArrayList<String>();
 	}
 
-	public void setArrayDatos(ArrayList<Medida> arrayDatos) {
-		this.arrayDatos = arrayDatos;
+	public void add_dato(double r1, double r2, double r3, double v1, double v2, double v3){
+		medida = r1 + "," + r2 + "," + r3 + "," + v1 + "," + v2 + "," + v3;
+		arrayMedidas.add(medida);
 	}
-	/**
-	 * A partir del indice devuelve el valor de la medida correspondiente  
-	 * @param indice Numero correspondiente a la posicion de la
-	 * @return
-	 */
-	public Medida get_medida(int indice){
-		//TODO control de errores
-		return arrayDatos.get(indice);
+
+	public static Datos getDatos(){
+		if(datos == null) return new Datos();
+		return datos;
+	}
+
+	public String get_medida(int indice){
+		return arrayMedidas.get(indice);
+	}
+
+	public ArrayList<String> getArray(){
+		return arrayMedidas;
+	}
+
+	public void add(String item){
+		arrayMedidas.add(item);
+	}
+
+	public void remove(){
+		if(arrayMedidas.size()>0) arrayMedidas.remove(arrayMedidas.size()-1);
 	}
 	
 	/**
@@ -81,7 +55,7 @@ public class Datos {
 	 * @return 0 Si se ha creado el archivo 1 Si la SD no esta montada, 2 si existe un archivo con el nombre de la carpeta, 3 Si no se ha podido crear el archivo
 	 * 4 si no se soprta el "encoding" 
 	 * 
-	 */
+	 *
 	public int exportar_csv(){
 		
 		Calendar cal = Calendar.getInstance();
@@ -121,7 +95,7 @@ public class Datos {
 	/**
 	 * Genera la cadena de caracteres necesaria para la creación de un archivo CSV estandar.
 	 * @return String con todas las medidas de los Datos, siguiendo el estandar de los archivos CSV.
-	 */
+	 *
 	private String crearCSV(){
 		
 		if (apartado==1){
@@ -141,34 +115,12 @@ public class Datos {
 			return cadenaDatos;	
 		}
 	}
-	/**
-	 * Para la medida dada se comprueba si existe otra igual
-	 * en el conjunto.
-	 * @param medida Medida que se desa comprobar
-	 */
-	public boolean esta(Medida medida){
-		for(Medida med : arrayDatos){
-			if(medida.valor1 == med.valor1){
-				return true;
-			}
-		}
-		return false;
-	}
 	
 	/**
 	 * Metodo que da el tamaño del conjunto.
 	 * @return Cantidad de datos que tiene el conjunto.
 	 */
 	public int length(){
-		return arrayDatos.size();
-	}
-
-	public void set_lleno(boolean b) {
-		// TODO Auto-generated method stub
-		this.lleno = b;
-	}
-	
-	public boolean esta_lleno(){
-		return lleno;
+		return arrayMedidas.size();
 	}
 }
--- app/src/main/java/es/uva/inf/kave/myapplication/Simulador.java
+++ app/src/main/java/es/uva/inf/kave/myapplication/Simulador.java
@@ -4,17 +4,72 @@ package es.uva.inf.kave.myapplication;
  * Created by coke on 12/05/15.
  */
 
+import es.uva.inf.kave.myapplication.Datos;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Simulador extends Fragment {
+public class Simulador extends Fragment{
+
+    private EditText r1;
+    private EditText r2;
+    private EditText r3;
+    private EditText v1;
+    private EditText v2;
+    private EditText v3;
+    private Button medir;
+    private Button deshacer;
+    private Button limpiar;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.simulador, container, false);
+
+        r1 = (EditText) rootView.findViewById(R.id.r1);
+        r2 = (EditText) rootView.findViewById(R.id.r2);
+        r3 = (EditText) rootView.findViewById(R.id.r3);
+        v1 = (EditText) rootView.findViewById(R.id.v1);
+        v2 = (EditText) rootView.findViewById(R.id.v2);
+        v3 = (EditText) rootView.findViewById(R.id.v3);
+        medir = (Button) rootView.findViewById(R.id.Medir);
+        deshacer = (Button) rootView.findViewById(R.id.deshacer);
+        limpiar = (Button) rootView.findViewById(R.id.limpiar);
+
+        medir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String item = r1+","+r2+","+r3+","+v1+","+v2+","+v3;
+                Datos.getDatos().add(item);
+            }
+        });
+
+        deshacer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Datos.getDatos().remove();
+            }
+        });
+
+        limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((EditText) getView().findViewById(R.id.r1)).setText("");
+                ((EditText) getView().findViewById(R.id.r2)).setText("");
+                ((EditText) getView().findViewById(R.id.r3)).setText("");
+                ((EditText) getView().findViewById(R.id.v1)).setText("");
+                ((EditText) getView().findViewById(R.id.v2)).setText("");
+                ((EditText) getView().findViewById(R.id.v3)).setText("");
+            }
+        });
+
         return rootView;
+
     }
 }
