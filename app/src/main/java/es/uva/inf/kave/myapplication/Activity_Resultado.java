8ac086b7b08728298c73943911bbc4ca525efd61
coke727 <jorge.cuadrado.saez@gmail.com>
2015-05-30 17:47:34 +0200

    Añadida vista de resultados

---
+++ app/src/main/java/es/uva/inf/kave/myapplication/Activity_Resultado.java
@@ -0,0 +1,16 @@
+package es.uva.inf.kave.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by coke on 27/05/15.
+ */
+public class Activity_Resultado extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultado);
+    }
+}
